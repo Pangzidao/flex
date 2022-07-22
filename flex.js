86acd6d444cbb7484037a0e1c8b312685fbb7500
Pangzidao <102028262+Pangzidao@users.noreply.github.com>
2022-07-22 19:38:51 +0200

    version 0.1

--- flex.js
+++ flex.js
@@ -1,1 +1,21 @@
 console.log("hello");
+
+/* DOM elements*/
+
+const title = document.getElementsByTagName("h1");
+const container = document.getElementsByTagName("section");
+const box = document.getElementsByTagName("div");
+const btn = document.querySelectorAll("button");
+
+console.log(btn[3]);
+
+btn.forEach((item)=>{
+    item.addEventListener("click",function(e){
+        let btnId = e.target.id;
+        let btnClass = e.target.className;
+        container[0].style[btnClass] = btnId;
+    });
+});
+
+
+
